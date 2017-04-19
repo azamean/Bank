97c86499c4b6685f80bc74f061a0760e4e55c050
azamean <marcusobyrne@gmail.com>
2017-04-19 22:22:39 +0100

    Added text fields for data description

--- BankingApp/app/src/main/java/marcusobyrne/bankingapp/MainActivity.java
+++ BankingApp/app/src/main/java/marcusobyrne/bankingapp/MainActivity.java
@@ -1,7 +1,9 @@
 package marcusobyrne.bankingapp;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -25,12 +27,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        setTitle("");
+
+
 
 
     }
