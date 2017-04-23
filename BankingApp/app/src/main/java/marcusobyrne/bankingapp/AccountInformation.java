46a342dda2b4424cb3b061090e17d67ae56b02ce
azamean <marcusobyrne@gmail.com>
2017-04-23 17:02:29 +0100

    Added a a method to retrieve the saved data, and a new page for account information

---
+++ BankingApp/app/src/main/java/marcusobyrne/bankingapp/AccountInformation.java
@@ -0,0 +1,14 @@
+package marcusobyrne.bankingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AccountInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_information);
+        setTitle("");
+    }
+}
--- BankingApp/app/src/main/java/marcusobyrne/bankingapp/MainMenu.java
+++ BankingApp/app/src/main/java/marcusobyrne/bankingapp/MainMenu.java
@@ -11,6 +11,15 @@ public class MainMenu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
         setTitle("");
+
+        Bundle bundle = getIntent().getExtras();
+        int ID = bundle.getInt("ID");
+        String name = bundle.getString("name");
+        String addressLine1 = bundle.getString("addressLine1");
+        String addressLine2 = bundle.getString("addressLine2");
+        int accNo = bundle.getInt("accNo");
+        int PIN = bundle.getInt("PIN");
+        double balance = bundle.getDouble("balance");
     }
 
     public void viewAccount(View view){
