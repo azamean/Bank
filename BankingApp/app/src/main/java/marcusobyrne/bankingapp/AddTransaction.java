3d77a322123108cab68a7124725fc3963988c800
azamean <marcusobyrne@gmail.com>
2017-04-23 20:45:20 +0100

    User can now add transactions

--- BankingApp/app/src/main/java/marcusobyrne/bankingapp/AddTransaction.java
+++ BankingApp/app/src/main/java/marcusobyrne/bankingapp/AddTransaction.java
@@ -1,7 +1,11 @@
 package marcusobyrne.bankingapp;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 public class AddTransaction extends AppCompatActivity {
@@ -17,9 +21,33 @@ public class AddTransaction extends AppCompatActivity {
         setContentView(R.layout.activity_add_transaction);
         setTitle("");
 
+        Description = (EditText) findViewById(R.id.editTextDecription);
+        Amount = (EditText) findViewById(R.id.editTextAmount);
+
+        dbHandler = new DBHandler(this, null, null, 1);
+
     }
 
     public void addTransaction(){
+            Transaction transaction = new Transaction(Description.getText().toString(),
+                    Double.valueOf(Amount.getText().toString()));
 
+            dbHandler.addTransaction(transaction);
+
+            final Intent transactionAdded = new Intent(this, MainMenu.class);
+
+            AlertDialog alertDialog = new AlertDialog.Builder(AddTransaction.this).create();
+            alertDialog.setTitle("Success");
+            alertDialog.setMessage("Transaction has been added successfully");
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                            startActivity(transactionAdded);
+                        }
+                    });
+            alertDialog.show();
+
+        
     }
 }
