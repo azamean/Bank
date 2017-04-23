a692f81b9afd3d82af09d1a3604cc889d3eedfb5
azamean <marcusobyrne@gmail.com>
2017-04-23 17:15:59 +0100

    Added a page to display users account information

--- BankingApp/app/src/main/java/marcusobyrne/bankingapp/AccountInformation.java
+++ BankingApp/app/src/main/java/marcusobyrne/bankingapp/AccountInformation.java
@@ -2,13 +2,38 @@ package marcusobyrne.bankingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AccountInformation extends AppCompatActivity {
+
+    TextView textName = (TextView) findViewById(R.id.textViewName);
+    TextView textAddress1 = (TextView) findViewById(R.id.textViewAddress1);
+    TextView textAddress2 = (TextView) findViewById(R.id.textViewAddress2);
+    TextView textAccNo = (TextView) findViewById(R.id.textViewAccNo);
+    TextView textPIN = (TextView) findViewById(R.id.textViewPIN);
+    TextView textBalance = (TextView) findViewById(R.id.textViewBalance);
+
+
+    Bundle bundle = getIntent().getExtras();
+    int ID = bundle.getInt("ID");
+    String name = bundle.getString("name");
+    String addressLine1 = bundle.getString("addressLine1");
+    String addressLine2 = bundle.getString("addressLine2");
+    int accNo = bundle.getInt("accNo");
+    int PIN = bundle.getInt("PIN");
+    double balance = bundle.getDouble("balance");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_information);
         setTitle("");
+
+        textName.setText(name);
+        textAddress1.setText(addressLine1);
+        textAddress2.setText(addressLine2);
+        textAccNo.setText(accNo);
+        textPIN.setText(PIN);
+        textBalance.setText((int) balance);
     }
 }
--- BankingApp/app/src/main/java/marcusobyrne/bankingapp/MainMenu.java
+++ BankingApp/app/src/main/java/marcusobyrne/bankingapp/MainMenu.java
@@ -1,10 +1,22 @@
 package marcusobyrne.bankingapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainMenu extends AppCompatActivity {
+
+
+
+    Bundle bundle = getIntent().getExtras();
+    int ID = bundle.getInt("ID");
+    String name = bundle.getString("name");
+    String addressLine1 = bundle.getString("addressLine1");
+    String addressLine2 = bundle.getString("addressLine2");
+    int accNo = bundle.getInt("accNo");
+    int PIN = bundle.getInt("PIN");
+    double balance = bundle.getDouble("balance");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,17 +24,18 @@ public class MainMenu extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
         setTitle("");
 
-        Bundle bundle = getIntent().getExtras();
-        int ID = bundle.getInt("ID");
-        String name = bundle.getString("name");
-        String addressLine1 = bundle.getString("addressLine1");
-        String addressLine2 = bundle.getString("addressLine2");
-        int accNo = bundle.getInt("accNo");
-        int PIN = bundle.getInt("PIN");
-        double balance = bundle.getDouble("balance");
     }
 
     public void viewAccount(View view){
+        Intent viewAcc = new Intent(this, MainMenu.class);
+        viewAcc.putExtra("ID", ID);
+        viewAcc.putExtra("name", name);
+        viewAcc.putExtra("addressLine1", addressLine1);
+        viewAcc.putExtra("addressLine2", addressLine2);
+        viewAcc.putExtra("accNo", accNo);
+        viewAcc.putExtra("PIN", PIN);
+        viewAcc.putExtra("balance", balance);
+        startActivity(viewAcc);
 
     }
 }
